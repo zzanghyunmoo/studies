6df8d53fb891df8672fe12eaaea727c93723ddf4
장현우 <zzanghyunmoo@gmail.com>
2025-08-19 00:34:02 +0900

    [PocuCsharp] feat: Add Outpu project

---
+++ 00_project/PocuCsharp/00/Output/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Output;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(30);              // int
+        Console.WriteLine(30.1);            // double
+        Console.WriteLine(30.2f);           // float
+        Console.WriteLine(true);            // bool
+        Console.WriteLine('p');             // char
+        Console.WriteLine("Hello World!");  // string
+    }
+}
