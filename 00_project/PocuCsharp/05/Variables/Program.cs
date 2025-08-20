3e314d807f91eb4d61c939c5190dd55445f66d34
장현우 <zzanghyunmoo@gmail.com>
2025-08-20 21:50:08 +0900

    [PocuCsharp] feat: Update variables
    챕터에 맞게 변수 사용하는 코드로 변경

--- 00_project/PocuCsharp/05/Variables/Program.cs
+++ 00_project/PocuCsharp/05/Variables/Program.cs
@@ -4,12 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        const float NUM_1 = 10.0f;
-        const float NUM_2 = 20.0f;
+        float num1 = 10.0f;
+        float num2 = 20.0f;
 
-        Console.WriteLine("{0} + {1} = {2}", NUM_1, NUM_2, NUM_1 + NUM_2);
-        Console.WriteLine("{0} - {1} = {2}", NUM_1, NUM_2, NUM_1 - NUM_2);
-        Console.WriteLine("{0} * {1} = {2}", NUM_1, NUM_2, NUM_1 * NUM_2);
-        Console.WriteLine("{0} / {1} = {2}", NUM_1, NUM_2, NUM_1 / NUM_2);
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+        Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+
     }
 }
