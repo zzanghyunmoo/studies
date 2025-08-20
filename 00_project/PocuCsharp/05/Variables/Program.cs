e11f4f281d98b435a26df2bb509febf77ac19cae
장현우 <zzanghyunmoo@gmail.com>
2025-08-20 21:48:09 +0900

    [PocuCsharp] feat: Variables 추가

---
+++ 00_project/PocuCsharp/05/Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const float NUM_1 = 10.0f;
+        const float NUM_2 = 20.0f;
+
+        Console.WriteLine("{0} + {1} = {2}", NUM_1, NUM_2, NUM_1 + NUM_2);
+        Console.WriteLine("{0} - {1} = {2}", NUM_1, NUM_2, NUM_1 - NUM_2);
+        Console.WriteLine("{0} * {1} = {2}", NUM_1, NUM_2, NUM_1 * NUM_2);
+        Console.WriteLine("{0} / {1} = {2}", NUM_1, NUM_2, NUM_1 / NUM_2);
+    }
+}
