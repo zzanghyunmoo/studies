a027fbbc017e755a967cb1e3cc428cba6673da93
장현우 <zzanghyunmoo@gmail.com>
2025-08-23 23:58:48 +0900

    [PocuCsharp] feat: Add example bit flag

---
+++ 00_project/PocuCsharp/07/BitFlag/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BitFlag;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int BIT_FLAG_SIZE = 8;
+        byte bitFlags = 0b0000_0000;
+        byte mask1 = 0b0000_0001 << 2; // 0000_0100
+
+        bitFlags |= mask1;
+        Console.WriteLine(Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+
+        byte mask2 = (0b0000_0001 << 3) | (0b0000_0001 << 5); // 0010_1000
+        bitFlags |= mask2; // 0010_1100
+        Console.WriteLine(Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+
+        bitFlags &= (byte)~mask1;
+        Console.WriteLine(Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+
+        bitFlags &= 0;
+        Console.WriteLine(Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+    }
+}
--- 00_project/PocuCsharp/07/BitShiftOperator/Program.cs
+++ 00_project/PocuCsharp/07/BitShiftOperator/Program.cs
@@ -11,5 +11,24 @@ class Program
         Console.WriteLine($"b: {Convert.ToString(b, 2).PadLeft(4, '0')}");
         Console.WriteLine($"left: {Convert.ToString(left, 2).PadLeft(4, '0')}");
         Console.WriteLine($"right: {Convert.ToString(right, 2).PadLeft(4, '0')}");
+
+        int num = 1;
+        int res1 = num << 1;
+        Console.WriteLine(res1); // 2
+
+        int res2 = num << 2;
+        Console.WriteLine(res2); // 4
+
+        int res3 = num << 3;
+        Console.WriteLine(res3); // 8
+
+        int res4 = res3 >> 1;
+        Console.WriteLine(res4); // 4
+
+        int res5 = res3 >> 2;
+        Console.WriteLine(res5); // 2
+
+        int res6 = res3 >> 3;
+        Console.WriteLine(res6); // 1
     }
 }
