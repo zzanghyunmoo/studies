115209eacb38ff29f638b168903073bc0088d344
장현우 <zzanghyunmoo@gmail.com>
2025-08-23 23:33:52 +0900

    [PocuCsharp] feat: Add Example Bit Operator

---
+++ 00_project/PocuCsharp/06/BitOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BitOperator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte b1 = 0b1100;
+        byte b2 = 0b1001;
+
+        // output format binary format 
+        Console.WriteLine(Convert.ToString(b1 & b2, 2).PadLeft(8, '0'));  
+        Console.WriteLine(Convert.ToString(b1 | b2, 2).PadLeft(8, '0'));  
+        Console.WriteLine(Convert.ToString(b1 ^ b2, 2).PadLeft(8, '0'));  
+        Console.WriteLine(Convert.ToString(~b2, 2).Substring(24, 8));  
+        Console.WriteLine("Hello, World!");
+    }
+}
