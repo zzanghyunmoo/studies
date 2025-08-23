f555d15964c5656a299264ac98c9736eac7a8265
장현우 <zzanghyunmoo@gmail.com>
2025-08-23 23:14:41 +0900

    [PocuCsharp] feat: Add example Counter

---
+++ 00_project/PocuCsharp/06/CounterOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CounterOperator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 10;
+        int res1 = i++;
+        Console.WriteLine($"i: {i}, res: {res1}"); // i: 11, res: 10
+        int res2 = ++i;
+        Console.WriteLine($"i: {i}, res: {res2}"); // i: 12, res: 12
+
+        int res3 = i--;
+        Console.WriteLine($"i: {i}, res: {res3}"); // i: 11, res: 12
+        int res4 = --i;
+        Console.WriteLine($"i: {i}, res: {res4}"); // i: 10, res: 10
+    }
+}
