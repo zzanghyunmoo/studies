94d408010a18e20a4350272f54b7568e85844243
장현우 <zzanghyunmoo@gmail.com>
2025-08-23 22:56:26 +0900

    [PocuCharp] feat: Add Example Math Operator

---
+++ 00_project/PocuCsharp/06/MathOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MathOperator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 100;
+        int b = 21;
+
+        // Addition
+        int add = a + b;
+        Console.WriteLine($"Add: {add}");
+
+        // Subtraction
+        int sub = a - b;
+        Console.WriteLine($"Substraction: {sub}");
+
+        // Multiplication
+        int mul = a * b;
+        Console.WriteLine($"Multiplication: {mul}");
+
+        // Division
+        double div = (double)a / b;
+        Console.WriteLine($"Division: {div}");
+
+        // Modulus
+        int mod = a % b;
+        Console.WriteLine($"Modulus: {mod}");
+    }
+}
