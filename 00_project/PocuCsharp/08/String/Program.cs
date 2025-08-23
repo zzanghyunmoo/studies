f442ef2c63d2325e658e226784690819e58011cb
장현우 <zzanghyunmoo@gmail.com>
2025-08-24 00:40:36 +0900

    [PocuCsharp] feat: Update Example String \"\"

--- 00_project/PocuCsharp/08/String/Program.cs
+++ 00_project/PocuCsharp/08/String/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string greeting = "Hello~";
+        string greeting = "Hello~ \"Kim Pope\"";
         Console.WriteLine(greeting);
     }
 }
