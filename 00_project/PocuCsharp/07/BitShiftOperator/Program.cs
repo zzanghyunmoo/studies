db115733d5fc9aff2036299cdc680917bc6ae647
장현우 <zzanghyunmoo@gmail.com>
2025-08-23 23:42:43 +0900

    [PocuCshart] feat: Add Example Bit shift Operator
    1. Bit shift 연산자 예제 추가
    2. Bit 연산자 디렉토리 이동

---
+++ 00_project/PocuCsharp/07/BitShiftOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BitShiftOperator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int b = 0b0010;     // 2
+        int left = b << 1;  // 4
+        int right = b >> 1; // 1
+
+        Console.WriteLine($"b: {Convert.ToString(b, 2).PadLeft(4, '0')}");
+        Console.WriteLine($"left: {Convert.ToString(left, 2).PadLeft(4, '0')}");
+        Console.WriteLine($"right: {Convert.ToString(right, 2).PadLeft(4, '0')}");
+    }
+}
