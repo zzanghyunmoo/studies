bcd801b6be30f87fdedd7c25e5d3c432b3bc92fb
장현우 <zzanghyunmoo@gmail.com>
2025-08-24 01:05:29 +0900

    [PocuCsharp] feat: Add example String Format

--- 00_project/PocuCsharp/08/String/Program.cs
+++ 00_project/PocuCsharp/08/String/Program.cs
@@ -6,5 +6,8 @@ class Program
     {
         string greeting = "Hello~ \"Kim Pope\"";
         Console.WriteLine(greeting);
+
+        // AscII 로 Hello World 출력하기
+        Console.WriteLine("\x48\x65\x6C\x6C\x6F\x2C\x20\x57\x6F\x72\x6C\x64\x21");
     }
 }
---
+++ 00_project/PocuCsharp/08/StringFormatting/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StringFormatting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Lulu";
+        int id = 20250001;
+        string message = string.Format("Name: {0}, ID: {1}", name, id);
+        Console.WriteLine(message);
+        Console.WriteLine("Name: {0}, ID: {1}", name, id);
+        Console.WriteLine($"Name: {name}, ID: {id}");
+
+        string student1 = "Kim Leon";
+        float winRate1 = 30.2351f;
+        string student2 = "Lulu";
+        float winRate2 = 70.11f;
+
+        Console.WriteLine("{0} {1}", "Name", "Win Rate");
+        Console.WriteLine("{0} {1}", student1, winRate1);
+        Console.WriteLine("{0} {1}", student2, winRate2);   
+
+        Console.WriteLine("{0,10}{1,15}", "Name", "Win Rate");
+        Console.WriteLine("{0,10}{1,15:f1}", student1, winRate1);
+        Console.WriteLine("{0,10}{1,15:f1}", student2, winRate2);
+    }
+}
