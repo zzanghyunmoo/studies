45be0e6de9f9e0814b452eddbc07f2fdd35acb79
장현우 <zzanghyunmoo@gmail.com>
2025-08-24 18:45:46 +0900

    [PocuCsharp] feat: Update example Logical Expressions

--- 00_project/PocuCsharp/10/LogicalExpression/Program.cs
+++ 00_project/PocuCsharp/10/LogicalExpression/Program.cs
@@ -4,15 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        int num1 = 1;
-        int num2 = 1;
-        int num3 = 4;
+        Console.Write("Enter your score: ");
+        string input = Console.ReadLine() ?? "invalid";
+        if (input == "invalid")
+        {
+            Console.WriteLine("error: Invalid input. Please enter a valid number.");
+            return;
+        }
 
-        bool b1 = !(num1 == num2) && (num3 > num2);
-        bool b2 = (num1 < num2) || (num2 == num3);
-        bool b3 = (num1 > num2) || num1 == num3 || ++num1 > num2;
-        bool b4 = (num3 >= num2) || num1-- == num2;
-        bool b5 = num3 == num1 && --num2 > num1;
-        Console.WriteLine($"b1: {b1}, b2: {b2}, b3: {b3}, b4: {b4}, b5: {b5}");
+        if (int.TryParse(input, out int score))
+        {
+            // Console.WriteLine($"Your math score is {score}.");
+        }
+        else
+        {
+            Console.WriteLine("error: Invalid input. Please enter a valid number.");
+            return;
+        }
+
+        if (score >= 90)
+        {
+            Console.WriteLine("You got an Gaming box.");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("You got a Book.");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("You got a Pizza.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the math exam.");
+        }
     }
 }
