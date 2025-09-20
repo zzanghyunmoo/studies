bfe17ff0ce9522ce545bf05f96fc2a2be0f843a1
장현우 <zzanghyunmoo@gmail.com>
2025-09-20 22:29:06 +0900

    [LearnCPP] fix #1. Hello World .editorconfig 추가

--- 00_project/LearnCPP/HelloWorld/HelloWorld.cpp
+++ 00_project/LearnCPP/HelloWorld/HelloWorld.cpp
@@ -1,4 +1,4 @@
-﻿// HelloWorld.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// HelloWorld.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+	return 0;	
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
