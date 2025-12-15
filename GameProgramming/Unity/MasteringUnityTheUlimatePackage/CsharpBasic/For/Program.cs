db8f64c6f371d6d7bb6c0f4948be89661812933f
장현우 <zzanghyunmoo@gmail.com>
2025-12-15 23:06:07 +0900

    [MasteringUnity] feat: Add ch04 For (공부 전)

---
+++ GameProgramming/Unity/MasteringUnityTheUlimatePackage/CsharpBasic/For/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace For;
+
+class Gift
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    public Gift(string name, int price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var gifts = new Gift[]
+        {
+            new Gift("Teddy Bear", 20),
+            new Gift("Lego Set", 35),
+            new Gift("Puzzle", 15),
+            new Gift("Action Figure", 25)
+        };
+
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
